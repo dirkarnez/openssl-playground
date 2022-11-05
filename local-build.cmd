593fcdd41172a78a9c3e810d1919944add3ae4ac
dirkarnez <smalldirkalex@gmail.com>
2022-11-05 20:07:49 +0800

    - update cpk-cmake

--- local-build.cmd
+++ local-build.cmd
@@ -2,23 +2,27 @@ REM run as Administrator
 @echo off
 
 cd /d %~dp0
-@REM set DOWNLOAD_DIR=%USERPROFILE%\Downloads
-@REM set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
-@REM SET PATH=^
-@REM %DOWNLOAD_DIR%\PortableGit\bin;^
-@REM %DOWNLOAD_DIR%\x86_64-8.1.0-release-win32-seh-rt_v6-rev0;^
-@REM %DOWNLOAD_DIR%\x86_64-8.1.0-release-win32-seh-rt_v6-rev0\bin;^
-@REM %DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;^
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+set DOWNLOADS_DIR_LINUX=%DOWNLOAD_DIR:\=/%
+SET PATH=^
+%DOWNLOADS_DIR%\PortableGit\bin;^
+%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0;^
+%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\bin;^
+%DOWNLOADS_DIR%\cmake-3.22.2-windows-x86_64\bin;^
 
-set PATH=^
-D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
-D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
-D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
+@REM set PATH=^
+@REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+@REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
+@REM D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
+
+
+set current_directory=%~dp0
+set current_directory_linux=%current_directory:\=/%
 
 rmdir build
 
 cmake.exe -G"MinGW Makefiles" ^
--DOpenSSL_DIR="D:/Code Projects/cpp-libraries/openssl-v3.0.5-x86_64-posix-seh-rev0-8.1.0" ^
+-DOpenSSL_DIR="%current_directory_linux%/cpk-cmake/external/openssl-v3.0.5-x86_64-posix-seh-rev0-8.1.0" ^
 -DCMAKE_BUILD_TYPE=Debug ^
 -B./build &&^
 cd build &&^
