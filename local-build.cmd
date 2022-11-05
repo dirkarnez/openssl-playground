73dfb53daecbd6c666be5c89792580319195b31b
dirkarnez <smalldirkalex@gmail.com>
2022-11-05 20:30:52 +0800

    - update cpk-cmake

--- local-build.cmd
+++ local-build.cmd
@@ -4,17 +4,16 @@ REM run as Administrator
 cd /d %~dp0
 set DOWNLOADS_DIR=%USERPROFILE%\Downloads
 set DOWNLOADS_DIR_LINUX=%DOWNLOAD_DIR:\=/%
-SET PATH=^
-%DOWNLOADS_DIR%\PortableGit\bin;^
-%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0;^
-%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\bin;^
-%DOWNLOADS_DIR%\cmake-3.22.2-windows-x86_64\bin;^
+@REM SET PATH=^
+@REM %DOWNLOADS_DIR%\PortableGit\bin;^
+@REM %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0;^
+@REM %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\bin;^
+@REM %DOWNLOADS_DIR%\cmake-3.22.2-windows-x86_64\bin;^
 
-@REM set PATH=^
-@REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
-@REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
-@REM D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
-
+set PATH=^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
+D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
 
 set current_directory=%~dp0
 set current_directory_linux=%current_directory:\=/%
